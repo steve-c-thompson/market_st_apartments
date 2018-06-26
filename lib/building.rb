6a0903256adb2268381aba5e4f759110dd3e5550
Steve Thompson <scthomps312@gmail.com>
2018-06-26 07:55:58 -0600

    Added functionality for iteration 3: buildings can add units and calculate rent and occupancy

--- lib/building.rb
+++ lib/building.rb
@@ -1,2 +1,38 @@
 class Building
+  attr_reader :units
+
+  def initialize
+    @units = []
+  end
+
+  def add_unit(apt)
+    @units.push(apt)
+  end
+
+  def average_rent
+    total_rent = calc_total_rent
+    if(total_rent == 0)
+      return total_rent
+    end
+    total_rent / @units.count
+  end
+
+  def total_annual_rent
+    12 * calc_total_rent
+  end
+
+  def occupancy
+    if(@units.count == 0)
+      return 0.0
+    end
+    residents = @units.reduce(0) do |sum, apt|
+      apt.renter ? sum + 1 : sum
+    end
+    (residents / @units.count.to_f) * 100 #order of ops would take care of this, but more readable
+  end
+
+  private
+  def calc_total_rent
+    total_rent = @units.reduce(0) { |sum, apt| sum + apt.monthly_rent }
+  end
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -4,4 +4,60 @@ require 'minitest/pride'
 require './lib/building'
 
 class BuildingTest < Minitest::Test
+  def test_it_exists
+    building = Building.new
+    assert_instance_of Building, building
+    assert_instance_of Array, building.units
+    assert_equal 0, building.units.count
+  end
+
+  def test_it_can_add_apartments
+    building = Building.new
+    a1 = Apartment.new({number: 1, monthly_rent: 650, bedroom: 1, bathroom: 1})
+    building.add_unit(a1)
+    assert_equal 1, building.units.count
+    assert_equal 1, building.units[0].number
+  end
+
+  def test_it_can_calculate_average_rent
+    building = Building.new
+    a1 = Apartment.new({number: 1, monthly_rent: 650, bedroom: 1, bathroom: 1})
+    building.add_unit(a1)
+    a2 = Apartment.new({number: 1, monthly_rent: 750, bedrooms: 0, bathrooms: 1})
+    building.add_unit(a2)
+
+    assert_equal 700, building.average_rent
+  end
+
+  def test_it_can_calculate_average_rent_for_no_units_returning_zero
+    building = Building.new
+    assert_equal 0, building.average_rent
+  end
+
+  def test_total_annual_rent_returns_total_rent_mult_12
+    building = Building.new
+    a1 = Apartment.new({number: 1, monthly_rent: 650, bedroom: 1, bathroom: 1})
+    building.add_unit(a1)
+    a2 = Apartment.new({number: 1, monthly_rent: 750, bedrooms: 0, bathrooms: 1})
+    building.add_unit(a2)
+
+    assert_equal 16800, building.total_annual_rent
+  end
+
+  def test_occupancy_returns_0_for_empty_building
+    building = Building.new
+    assert_equal 0.0, building.occupancy
+  end
+
+  def test_occupancy_returns_percentage_of_renters_divided_by_apartments
+      building = Building.new
+      a1 = Apartment.new({number: 1, monthly_rent: 650, bedroom: 1, bathroom: 1})
+      building.add_unit(a1)
+      a2 = Apartment.new({number: 1, monthly_rent: 750, bedrooms: 0, bathrooms: 1})
+      building.add_unit(a2)
+      assert_equal 0.0, building.occupancy
+      joe = Renter.new("Joe")
+      a1.add_renter(joe)
+      assert_equal 50.0, building.occupancy
+  end
 end
