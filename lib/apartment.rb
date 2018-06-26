c5560c867fb3c94bacf2bba91128e94661cd4de8
Steve Thompson <scthomps312@gmail.com>
2018-06-26 07:24:17 -0600

    Iteration 1, can create apartment

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,2 +1,13 @@
 class Apartment
+  attr_reader :number
+  attr_reader :monthly_rent
+  attr_reader :bedrooms
+  attr_reader :bathrooms
+
+  def initialize(data)
+    @number = data[:number]
+    @monthly_rent = data[:monthly_rent]
+    @bedrooms = data[:bedrooms]
+    @bathrooms = data[:bathrooms]
+  end
 end
--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -4,4 +4,12 @@ require 'minitest/autorun'
 require './lib/apartment'
 
 class ApartmentTest < Minitest::Test
+  def test_it_exists_with_readers
+    apartment = Apartment.new({number: 1, monthly_rent: 800, bathrooms: 2, bedrooms: 3})
+    assert_instance_of Apartment, apartment
+    assert_equal 1, apartment.number
+    assert_equal 800, apartment.monthly_rent
+    assert_equal 2, apartment.bathrooms
+    assert_equal 3, apartment.bedrooms
+  end
 end
